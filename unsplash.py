55681a4ea7ab0e510d9ad48080749739ab29f088
Michael Hacker <info@superbox.one>
2017-01-29 20:08:26 +0100

    Update Python-Wrapper

--- unsplash.py
+++ unsplash.py
@@ -1,12 +1,15 @@
 import json
 
-from urllib.request import urlopen
 from urllib.error   import URLError, HTTPError
+from urllib.parse   import urlencode
+from urllib.request import urlopen
 
 class Unsplash(object):
     def __init__(self, settings):
-        self._api_url        = 'https://api.unsplash.com/'
+        self._api_url        = 'https://api.unsplash.com'
+        self._oauth_url      = 'https://unsplash.com/oauth/authorize'
         self._application_id = settings['application_id']
+        self._callback_url   = settings['callback_url']
 
     def _get_json_data(self, url):
         try:
@@ -23,92 +26,140 @@ class Unsplash(object):
 
         return json_data
 
-    def get_users_profile(self, username):
-        url = self._api_url + 'users/' + username + '?client_id=' + self._application_id
+    def get_authentication_url(self, scope = ''):
+        if scope:
+            scope = ' '.join([item for item in scope])
 
-        return self._get_json_data(url)
+        query = {
+            'redirect_uri'  : self._callback_url,
+            'response_type' : 'code',
+            'client_id'     : self._application_id,
+            'scope'         : scope
+        }
+
+        url = self._oauth_url + '?' + urlencode(query)
+
+        return url
+
+    def get_users_profile(self, username):
+        url = self._api_url + '/users/' + username + '?'
+
+        query = {
+            'client_id' : self._application_id
+        }
+
+        return self._get_json_data(url + urlencode(query))
 
     def list_users_photos(self, username, page = 1, per_page = 10, order_by = 'latest'):
-        url  = self._api_url + 'users/' + username + '/photos'
-        url += '?page=' + str(page) + '&per_page=' + str(per_page) + '&order_by=' + order_by
-        url += '&client_id=' + self._application_id
+        url = self._api_url + '/users/' + username + '/photos?'
 
-        return self._get_json_data(url)
+        query = {
+            'page'      : page,
+            'per_page'  : per_page,
+            'order_by'  : order_by,
+            'client_id' : self._application_id
+        }
+
+        return self._get_json_data(url + urlencode(query))
     
     def list_users_likes(self, username, page = 1, per_page = 10, order_by = 'latest'):
-        url  = self._api_url + 'users/' + username + '/collections'
-        url += '?page=' + str(page) + '&per_page=' + str(per_page) + '&order_by=' + order_by
-        url += '&client_id=' + self._application_id
+        url = self._api_url + '/users/' + username + '/collections?'
 
-        return self._get_json_data(url)
+        query = {
+            'page'      : page,
+            'per_page'  : per_page,
+            'order_by'  : order_by,
+            'client_id' : self._application_id
+        }
+
+        return self._get_json_data(url + urlencode(query))
 
     def list_users_collections(self, username, page = 1, per_page = 10):
-        url  = self._api_url + 'users/' + username + '/collections'
-        url += '?page=' + str(page) + '&per_page=' + str(per_page)
-        url += '&client_id=' + self._application_id
+        url = self._api_url + '/users/' + username + '/collections?'
 
-        return self._get_json_data(url)
+        query = {
+            'page'      : page,
+            'per_page'  : per_page,
+            'client_id' : self._application_id
+        }
+
+        return self._get_json_data(url + urlencode(query))
 
     def list_photos(self, page = 1, per_page = 10, order_by = 'latest'):
-        url  = self._api_url + 'photos/'
-        url += '?page=' + str(page) + '&per_page=' + str(per_page) + '&order_by=' + order_by
-        url += '&client_id=' + self._application_id
+        url = self._api_url + '/photos/?'
 
-        return self._get_json_data(url)
+        query = {
+            'page'      : page,
+            'per_page'  : per_page,
+            'order_by'  : order_by,
+            'client_id' : self._application_id
+        }
+
+        return self._get_json_data(url + urlencode(query))
 
     def list_curated_photos(self, page = 1, per_page = 10, order_by = 'latest'):
-        url  = self._api_url + 'photos/curated/'
-        url += '?page=' + str(page) + '&per_page=' + str(per_page) + '&order_by=' + order_by
-        url += '&client_id=' + self._application_id
+        url  = self._api_url + '/photos/curated/?'
 
-        return self._get_json_data(url)
+        query = {
+            'page'      : page,
+            'per_page'  : per_page,
+            'order_by'  : order_by,
+            'client_id' : self._application_id
+        }
+
+        return self._get_json_data(url + urlencode(query))
 
     def search_photos(self, query, page = 1, per_page = 10):
-        url  = self._api_url + 'search/photos/'
-        url += '?query=' + query + '&page=' + str(page) + '&per_page=' + str(per_page)
-        url += '&client_id=' + self._application_id
+        url  = self._api_url + '/search/photos/?'
 
-        return self._get_json_data(url)
+        query = {
+            'query'     : query,
+            'per_page'  : per_page,
+            'client_id' : self._application_id
+        }
+
+        return self._get_json_data(url + urlencode(query))
 
     def get_photo(self, id= '', width = '', height= '', rectangle= ''):
-        if not id:
-            return 'The photo’s ID is required.'
+        url = self._api_url + '/photos/' + id + '?'
+        
+        query = {
+            'w'         : width,
+            'h'         : height,
+            'rect'      : rectangle,
+            'client_id' : self._application_id
+        }
 
-        url  = self._api_url + 'photos/' + id
-        url += '?w=' + str(width) + '&h=' + str(height) + '&rect=' + rectangle
-        url += '&client_id=' + self._application_id
-
-        return self._get_json_data(url)
+        return self._get_json_data(url + urlencode(query))
 
     def get_photo_stats(self, id = ''):
-        if not id:
-            return 'The photo’s ID is required.'
+        url = self._api_url + '/photos/' + id + '/stats?'
+        
+        query = {
+            'client_id' : self._application_id
+        }
 
-        url  = self._api_url + 'photos/' + id + '/stats'
-        url += '?client_id=' + self._application_id
-
-        return self._get_json_data(url)
+        return self._get_json_data(url + urlencode(query))
 
     def get_random_photo(self, collections = '', featured = '', username = '', query = '', 
                          width = '', height = '', orientation = '', count = 1):
-        url  = self._api_url + 'photos/random'
+        url = self._api_url + '/photos/random?'
 
-        if count > 30:
-            count = 30
-
-        if collections:
-            url += '?collections=' + collections + '&'
-        elif query:
-            url += '?query=' + query + '&'
-        else:
-            url += '?'
-
-        url += 'featured=' + featured + '&username=' + username
-        url += '&w=' + width + '&h=' + height
+        query = {
+            'featured'  : featured,
+            'username'  : username,
+            'w'         : width,
+            'h'         : height,
+            'count'     : count > 30 and 30 or count,
+            'client_id' : self._application_id
+        }
 
         if orientation:
-            url += '&orientation=' + orientation
+            query['orientation'] = orientation
 
-        url += '&count=' + str(count) + '&client_id=' + self._application_id
+        if collections:
+            query['collections'] = collections
+        elif query:
+            query['query'] = query
 
-        return self._get_json_data(url)
+        return self._get_json_data(url + urlencode(query))
