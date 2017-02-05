b44b686466efd7bef096c098b800896b97d2b14e
Michael Hacker <info@superbox.one>
2017-02-05 15:21:57 +0100

    Added list_curated_collections()

--- src/collections.py
+++ src/collections.py
@@ -11,13 +11,23 @@ class Collections(object):
         self._application_id = settings['application_id']
         self._api_url        = settings['api_url']
 
-    def list_collections(self, page = 1, per_page = 10, order_by = 'latest'):
+    def list_collections(self, page = 1, per_page = 10, order_by = 'popular'):
         url = self._api_url + '/collections'
 
         query = {
             'page'     : page,
             'per_page' : per_page,
             'order_by' : order_by
+        }
+
+        return Rest(self._application_id).get(url, query)
+
+    def list_curated_collections(self, page = 1, per_page = 10):
+        url = self._api_url + '/collections/featured'
+
+        query = {
+            'page'     : page,
+            'per_page' : per_page
         }
 
         return Rest(self._application_id).get(url, query)
