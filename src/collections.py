7ec4a007bfa7dc3082222994cc97f9d8e1b953e6
Michael Hacker <info@superbox.one>
2017-02-05 15:16:04 +0100

    Updated list_collections()

--- src/collections.py
+++ src/collections.py
@@ -11,12 +11,13 @@ class Collections(object):
         self._application_id = settings['application_id']
         self._api_url        = settings['api_url']
 
-    def list_collections(self, page = 1, per_page = 10):
+    def list_collections(self, page = 1, per_page = 10, order_by = 'latest'):
         url = self._api_url + '/collections'
 
         query = {
             'page'     : page,
-            'per_page' : per_page
+            'per_page' : per_page,
+            'order_by' : order_by
         }
 
         return Rest(self._application_id).get(url, query)
