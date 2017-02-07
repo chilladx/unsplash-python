b0349ebde2af4f734f0ed457497c8950a62b88a7
Michael Hacker <info@superbox.one>
2017-02-07 21:15:23 +0100

    Added wheels support

--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
-from setuptools import setup
+import uuid
+
+from pip.req import parse_requirements
+from setuptools import setup, find_packages
+
+install_requirements = parse_requirements('requirements.txt', session=uuid.uuid1())
+requirements = [str(req.req) for req in install_requirements]
 
 setup(
     name='unsplash-python',
     version='1.0.0a1',
     packages=['unsplash', 'unsplash.src'],
     description='A unofficial Python wrapper for the Unsplash API',
-    url='https://github.com/michael-hacker/unsplash-python/',
+    url='https://github.com/michael-hacker/unsplash-python.git',
     license='MIT',
     author='Michael Hacker',
+    author_email='mh@superchic.at',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.5'
     ],
-    keywords=['unsplash', 'api', 'python'],
-    install_requires=['requests']
+    keywords=['api', 'development', 'python', 'unsplash'],
+    install_requires=requirements
 )
