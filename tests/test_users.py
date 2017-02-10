3e9dffdaded7e6fc72cb8f0476f7a4ec4cc1d55a
Michael Hacker <mh@superchic.at>
2017-02-10 23:38:11 +0100

    Added unittest for current_users

--- tests/test_users.py
+++ tests/test_users.py
@@ -12,40 +12,40 @@ class CurrentUsers(UnsplashTestCase):
         self.assertIsInstance(response, dict)
         self.assertEqual(response.get('username'), 'michael_hacker')
 
-# class Users(UnsplashTestCase):
-#     def test_profile(self):
-#         response = self.unsplash.users().profile(
-#             username='michael_hacker'
-#         )
-#         self.assertIsInstance(response, dict)
-#         self.assertEqual(response.get('username'), 'michael_hacker')
+class Users(UnsplashTestCase):
+    def test_profile(self):
+        response = self.unsplash.users().profile(
+            username='michael_hacker'
+        )
+        self.assertIsInstance(response, dict)
+        self.assertEqual(response.get('username'), 'michael_hacker')
 
-#     def test_photos(self):
-#         response = self.unsplash.users().photos(
-#             username='michael_hacker',
-#             per_page=2
-#         )
-#         self.assertIsInstance(response, list)
-#         self.assertIsInstance(response[0], dict)
-#         self.assertEqual(len(response), 2)
+    def test_photos(self):
+        response = self.unsplash.users().photos(
+            username='michael_hacker',
+            per_page=2
+        )
+        self.assertIsInstance(response, list)
+        self.assertIsInstance(response[0], dict)
+        self.assertEqual(len(response), 2)
 
-#     def test_likes(self):
-#         response = self.unsplash.users().likes(
-#             username='michael_hacker',
-#             per_page=2
-#         )
-#         self.assertIsInstance(response, list)
-#         self.assertIsInstance(response[0], dict)
-#         self.assertEqual(len(response), 2)
+    def test_likes(self):
+        response = self.unsplash.users().likes(
+            username='michael_hacker',
+            per_page=2
+        )
+        self.assertIsInstance(response, list)
+        self.assertIsInstance(response[0], dict)
+        self.assertEqual(len(response), 2)
 
-#     def test_collections(self):
-#         response = self.unsplash.users().collections(
-#             username='michael_hacker',
-#             per_page=2
-#         )
-#         self.assertIsInstance(response, list)
-#         self.assertIsInstance(response[0], dict)
-#         self.assertEqual(len(response), 2)
+    def test_collections(self):
+        response = self.unsplash.users().collections(
+            username='michael_hacker',
+            per_page=2
+        )
+        self.assertIsInstance(response, list)
+        self.assertIsInstance(response[0], dict)
+        self.assertEqual(len(response), 2)
 
 
 if __name__ == "__main__":
