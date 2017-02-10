6c3a162ff72dfc5c7f83faa22cdfdad61c3741c0
Michael Hacker <mh@superchic.at>
2017-02-10 23:28:44 +0100

    Updated code to the PEP 8 (Style Guide) requirement

--- unsplash_python/src/users.py
+++ unsplash_python/src/users.py
@@ -1,19 +1,15 @@
-"""
-    GitHub: https://github.com/michael-hacker/unsplash-python
-    Author: Michael Hacker <mh@superchic.at>
-"""
-
-import logging
+# A unofficial Python wrapper for the Unsplash API.
+#
+# GitHub: https://github.com/michael-hacker/unsplash-python
+# Author: Michael Hacker <mh@superchic.at>
 
 from .rest import Rest
-
-logger = logging.getLogger('unsplash-python')
 
 
 class CurrentUsers(object):
     def __init__(self, settings):
         self._application_id = settings['application_id']
-        self._bearer_token   = settings['bearer_token']
+        self._bearer_token = settings['bearer_token']
 
     def profile(self):
         url = '/me?access_token=%s' % self._bearer_token
@@ -21,7 +17,7 @@ class CurrentUsers(object):
         return Rest().get(url)
 
     def update_profile(self, options):
-        # Work in progress
+        # Work in progress!
 
         url = '/me'
 
@@ -39,34 +35,34 @@ class Users(object):
 
         return Rest(self._application_id).get(url)
 
-    def photos(self, username, page = None, per_page = None, order_by = None):
+    def photos(self, username, page=None, per_page=None, order_by=None):
         url = '/users/%s/photos' % username
 
         query = {
-            'page'     : page,
-            'per_page' : per_page,
-            'order_by' : order_by
+            'page': page,
+            'per_page': per_page,
+            'order_by': order_by
         }
 
         return Rest(self._application_id).get(url, query)
 
-    def likes(self, username, page = None, per_page = None, order_by = None):
+    def likes(self, username, page=None, per_page=None, order_by=None):
         url = '/users/%s/likes' % username
 
         query = {
-            'page'     : page,
-            'per_page' : per_page,
-            'order_by' : order_by
+            'page': page,
+            'per_page': per_page,
+            'order_by': order_by
         }
 
         return Rest(self._application_id).get(url, query)
 
-    def collections(self, username, page = None, per_page = None):
+    def collections(self, username, page=None, per_page=None):
         url = '/users/%s/collections' % username
 
         query = {
-            'page'     : page,
-            'per_page' : per_page
+            'page': page,
+            'per_page': per_page
         }
 
         return Rest(self._application_id).get(url, query)
