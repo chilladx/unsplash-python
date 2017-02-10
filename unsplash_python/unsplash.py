35bbb30051a6ede6b064d75af497601bdd49c22a
Michael Hacker <mh@superchic.at>
2017-02-10 18:28:51 +0100

    Updated settings

--- unsplash_python/unsplash.py
+++ unsplash_python/unsplash.py
@@ -3,20 +3,20 @@
 # GitHub: https://github.com/michael-hacker/unsplash-python
 # Author: Michael Hacker <mh@superchic.at>
 
-from unsplash_python.src.rest import Rest
-from unsplash_python.src.auth import Auth
-from unsplash_python.src.users import CurrentUsers, Users
-from unsplash_python.src.photos import Photos
-from unsplash_python.src.collections import Collections
+from .src.rest import Rest
+from .src.auth import Auth
+from .src.users import CurrentUsers, Users
+from .src.photos import Photos
+from .src.collections import Collections
 
 
 class Unsplash(object):
     def __init__(self, settings):
         self._settings = {
-            'application_id': settings.get('application_id'),
-            'secret': settings.get('secret'),
-            'callback_url': settings.get('callback_url'),
-            'bearer_token': settings.get('bearer_token')
+            'application_id': settings.get('application_id', ''),
+            'secret': settings.get('secret', ''),
+            'callback_url': settings.get('callback_url', ''),
+            'bearer_token': settings.get('bearer_token', '')
         }
 
     def auth(self):
