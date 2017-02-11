70e86d7eaac8aed8364c3baa2e1a9f437ccb0747
Michael Hacker <mh@superchic.at>
2017-02-11 17:53:26 +0100

    Added vcr cassette

--- tests/test_collections.py
+++ tests/test_collections.py
@@ -2,11 +2,14 @@
 
 import unittest
 
+import vcr
+
 from config import UnsplashTestCase
 from unsplash_python.unsplash import Unsplash
 
 
 class Collections(UnsplashTestCase):
+    @vcr.use_cassette('tests/vcr/list_collections.json')
     def test_list_collections(self):
         response = self.unsplash.collections().list_collections(
             per_page=2
@@ -15,50 +18,56 @@ class Collections(UnsplashTestCase):
         self.assertIsInstance(response[0], dict)
         self.assertEqual(len(response), 2)
 
-    # def test_list_featured_collections(self):
-    #     response = self.unsplash.collections().list_featured_collections(
-    #         per_page=2
-    #     )
-    #     self.assertIsInstance(response, list)
-    #     self.assertIsInstance(response[0], dict)
-    #     self.assertEqual(len(response), 2)
+    @vcr.use_cassette('tests/vcr/list_featured_collections.json')
+    def test_list_featured_collections(self):
+        response = self.unsplash.collections().list_featured_collections(
+            per_page=2
+        )
+        self.assertIsInstance(response, list)
+        self.assertIsInstance(response[0], dict)
+        self.assertEqual(len(response), 2)
 
-    # def test_list_curated_collections(self):
-    #     response = self.unsplash.collections().list_curated_collections(
-    #         per_page=2
-    #     )
-    #     self.assertIsInstance(response, list)
-    #     self.assertIsInstance(response[0], dict)
-    #     self.assertEqual(len(response), 2)
+    @vcr.use_cassette('tests/vcr/list_curated_collections.json')
+    def test_list_curated_collections(self):
+        response = self.unsplash.collections().list_curated_collections(
+            per_page=2
+        )
+        self.assertIsInstance(response, list)
+        self.assertIsInstance(response[0], dict)
+        self.assertEqual(len(response), 2)
 
-    # def test_get_collection(self):
-    #     response = self.unsplash.collections().get_collection(
-    #         id=446398
-    #     )
-    #     self.assertIsInstance(response, dict)
-    #     self.assertEqual(response.get('id'), 446398)
+    @vcr.use_cassette('tests/vcr/get_collection.json')
+    def test_get_collection(self):
+        response = self.unsplash.collections().get_collection(
+            id=446398
+        )
+        self.assertIsInstance(response, dict)
+        self.assertEqual(response.get('id'), 446398)
 
-    # def test_get_curated_collection(self):
-    #     response = self.unsplash.collections().get_curated_collection(
-    #         id=134
-    #     )
-    #     self.assertIsInstance(response, dict)
+    @vcr.use_cassette('tests/vcr/get_curated_collection.json')
+    def test_get_curated_collection(self):
+        response = self.unsplash.collections().get_curated_collection(
+            id=134
+        )
+        self.assertIsInstance(response, dict)
 
-    # def test_get_collection_photos(self):
-    #     response = self.unsplash.collections().get_collection_photos(
-    #         id=446398,
-    #         per_page=2
-    #     )
-    #     self.assertIsInstance(response, list)
-    #     self.assertIsInstance(response[0], dict)
-    #     self.assertEqual(len(response), 2)
+    @vcr.use_cassette('tests/vcr/get_collection_photos.json')
+    def test_get_collection_photos(self):
+        response = self.unsplash.collections().get_collection_photos(
+            id=446398,
+            per_page=2
+        )
+        self.assertIsInstance(response, list)
+        self.assertIsInstance(response[0], dict)
+        self.assertEqual(len(response), 2)
 
-    # def test_get_curated_collection_photos(self):
-    #     response = self.unsplash.collections().get_curated_collection_photos(
-    #         id=134
-    #     )
-    #     self.assertIsInstance(response, list)
-    #     self.assertIsInstance(response[0], dict)
+    @vcr.use_cassette('tests/vcr/get_curated_collection_photos.json')
+    def test_get_curated_collection_photos(self):
+        response = self.unsplash.collections().get_curated_collection_photos(
+            id=134
+        )
+        self.assertIsInstance(response, list)
+        self.assertIsInstance(response[0], dict)
 
 
 if __name__ == '__main__':
