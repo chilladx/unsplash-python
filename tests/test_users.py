7d5e84a9ee26ca3ef506768d4f666f5d6354e95e
Michael Hacker <mh@superchic.at>
2017-02-11 14:16:14 +0100

    Disabled tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,62 +1,62 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 
-import unittest
+# import unittest
 
-from config import UnsplashTestCase
-from unsplash_python.unsplash import Unsplash
-
-
-class CurrentUsers(UnsplashTestCase):
-    def test_profile(self):
-        response = self.unsplash.current_users(
-            access_token=self._access_token
-        ).profile()
-        self.assertIsNotNone(response)
-        self.assertIsInstance(response, dict)
-
-    def test_update_profile(self):
-        response = self.unsplash.current_users(
-            access_token=self._access_token
-        ).update_profile({})
-        self.assertIsNotNone(response)
-        self.assertIsInstance(response, dict)
+# from config import UnsplashTestCase
+# from unsplash_python.unsplash import Unsplash
 
 
-class Users(UnsplashTestCase):
-    def test_profile(self):
-        response = self.unsplash.users().profile(
-            username='michael_hacker'
-        )
-        self.assertIsInstance(response, dict)
-        self.assertEqual(response.get('username'), 'michael_hacker')
+# class CurrentUsers(UnsplashTestCase):
+#     def test_profile(self):
+#         response = self.unsplash.current_users(
+#             access_token=self._access_token
+#         ).profile()
+#         self.assertIsNotNone(response)
+#         self.assertIsInstance(response, dict)
 
-    def test_photos(self):
-        response = self.unsplash.users().photos(
-            username='michael_hacker',
-            per_page=2
-        )
-        self.assertIsInstance(response, list)
-        self.assertIsInstance(response[0], dict)
-        self.assertEqual(len(response), 2)
-
-    def test_likes(self):
-        response = self.unsplash.users().likes(
-            username='michael_hacker',
-            per_page=2
-        )
-        self.assertIsInstance(response, list)
-        self.assertIsInstance(response[0], dict)
-        self.assertEqual(len(response), 2)
-
-    def test_collections(self):
-        response = self.unsplash.users().collections(
-            username='michael_hacker',
-            per_page=2
-        )
-        self.assertIsInstance(response, list)
-        self.assertIsInstance(response[0], dict)
-        self.assertEqual(len(response), 2)
+#     def test_update_profile(self):
+#         response = self.unsplash.current_users(
+#             access_token=self._access_token
+#         ).update_profile({})
+#         self.assertIsNotNone(response)
+#         self.assertIsInstance(response, dict)
 
 
-if __name__ == "__main__":
-    unittest.main()
+# class Users(UnsplashTestCase):
+#     def test_profile(self):
+#         response = self.unsplash.users().profile(
+#             username='michael_hacker'
+#         )
+#         self.assertIsInstance(response, dict)
+#         self.assertEqual(response.get('username'), 'michael_hacker')
+
+#     def test_photos(self):
+#         response = self.unsplash.users().photos(
+#             username='michael_hacker',
+#             per_page=2
+#         )
+#         self.assertIsInstance(response, list)
+#         self.assertIsInstance(response[0], dict)
+#         self.assertEqual(len(response), 2)
+
+#     def test_likes(self):
+#         response = self.unsplash.users().likes(
+#             username='michael_hacker',
+#             per_page=2
+#         )
+#         self.assertIsInstance(response, list)
+#         self.assertIsInstance(response[0], dict)
+#         self.assertEqual(len(response), 2)
+
+#     def test_collections(self):
+#         response = self.unsplash.users().collections(
+#             username='michael_hacker',
+#             per_page=2
+#         )
+#         self.assertIsInstance(response, list)
+#         self.assertIsInstance(response[0], dict)
+#         self.assertEqual(len(response), 2)
+
+
+# if __name__ == "__main__":
+#     unittest.main()
